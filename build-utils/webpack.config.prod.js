55d066a4eb02f4b572ab238a6fb263db8ed909f2
Mykhailo Kazakov <mykhailo_kazakov@epam.com>
2022-05-14 23:42:48 +0300

    Complete webpack configs v1.1

---
+++ build-utils/webpack.config.prod.js
@@ -0,0 +1,31 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+module.exports = () => ({
+  devtool: 'nosources-source-map',
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true,
+      }),
+      new OptimizeCSSAssetsPlugin({}),
+    ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.sa?css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+      chunkFilename: '[id].[contenthash].css',
+    }),
+  ],
+});
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,12 @@ const path = require('path');
 const webpack = require('webpack');
 const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const modeConfiguration = (env) => require(`./build-utils/webpack.config.${env}`)(env);
+
+function getConfigName(env) {
+  return env === 'development' ? 'webpack.config.dev' : 'webpack.config.prod';
+}
+
+const modeConfiguration = (env) => require(`./build-utils/${getConfigName(env)}`)(env);
 
 module.exports = ({ mode } = { mode: 'production' }) => {
   console.log(`mode is: ${mode}`);
@@ -10,15 +15,17 @@ module.exports = ({ mode } = { mode: 'production' }) => {
   return merge(
     {
       mode,
-      entry: './src/index.js',
+      entry: './src/index.jsx',
       devServer: {
         hot: true,
         open: true,
+        port: '3030',
       },
       output: {
-        publicPath: '/',
-        path: path.resolve(__dirname, 'build'),
-        filename: 'bundle.js',
+        path: path.resolve(__dirname, './build'),
+        filename: '[name].[hash].js',
+        chunkFilename: '[name].[chunkhash].js',
+        assetModuleFilename: 'images/[hash][ext][query]',
       },
       module: {
         rules: [
@@ -27,7 +34,15 @@ module.exports = ({ mode } = { mode: 'production' }) => {
             exclude: /node_modules/,
             loader: 'babel-loader',
           },
+          {
+            test: /\.(png|jpe?g|gif|svg|ico)$/,
+            exclude: /node_modules/,
+            use: ['url-loader', 'file-loader'],
+          },
         ],
+      },
+      resolve: {
+        extensions: ['.js', '.jsx'],
       },
       plugins: [
         new HtmlWebpackPlugin({
